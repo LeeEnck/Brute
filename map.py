cacad731d026f14f2c2332757b6cee3c7256844a
LeeEnck <55152412+LeeEnck@users.noreply.github.com>
2020-02-11 15:29:55 -0500

    Update map.py

--- map.py
+++ map.py
@@ -1,1 +1,20 @@
+from sys import exit
+from random import randint
+from textwrap import dedent
 
+
+#Start
+class area(object):
+
+    def play(self):
+        pass
+
+
+class Death(area):
+
+    def enter(self):
+        print(dedent("""
+            You have died
+            """))
+
+        exit(1)
